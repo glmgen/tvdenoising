c02a4c86be590953ba9a2123d0ed69834000ae8d
Daniel J. McDonald <dajmcdon@gmail.com>
2024-10-18 09:09:36 -0700

    normalize weights, better error message

--- R/tvdenoising.R
+++ R/tvdenoising.R
@@ -5,11 +5,12 @@
 #' Observation weights may be optionally specified.
 #'
 #' @param y Numeric vector of observations to be denoised.
-#' @param lambda Scalar. A non-negative regularization parameter.
-#' @param weights Numeric vector of the same length as y, containing
+#' @param lambda Scalar. A non-negative regularization parameter. These will be
+#'  rescaled to sum to `length(y)` internally.
+#' @param weights Numeric vector of the same length as `y`, containing
 #' 	observation weights. The default is `NULL`, which corresponds to unity
 #'  weights.
-#' @return Denoised observations.
+#' @return Denoised observations. A vector the same length as `y`
 #'
 #' @references Johnson (2013), "A dynamic programming algorithm for the fused
 #'  lasso and L0-segmentation."
@@ -17,17 +18,33 @@
 #' @examples
 #' y <- c(rnorm(30), rnorm(40, 2), rnorm(30))
 #' yhat <- tvdenoising(y, 10)
+#' w <- runif(100)
+#' yhat2 <- tvdenoising(y, 10, w)
 #' plot(y, pch = 16)
 #' lines(yhat, col = 2)
-tvdenoising <- function(y, lambda, weights=NULL) {
-  stopifnot(is.numeric(y))
-  stopifnot(is.numeric(lambda) & (length(lambda) == 1))
-  stopifnot(lambda >= 0)
-  if (is.null(weights)) {
-    return(flsa_dp(y, lambda))
-  } else {
-    stopifnot(is.numeric(weights))
-    stopifnot(length(weights) == length(y))
-    return(flsa_dp_weighted(y, lambda, weights))
+#' lines(yhat2, col = 4)
+tvdenoising <- function(y, lambda, weights = NULL) {
+  if (!is.numeric(y)) {
+    stop("`y` must be a numeric vector.")
   }
+  if (!is.numeric(lambda)) {
+    stop("`lambda` must be a number > 0.")
+  }
+  if (length(lambda) > 1) {
+    stop("`lambda` must be a scalar (have length 1).")
+  }
+  if (lambda < 0) {
+    stop("`lambda` must be non-negative.")
+  }
+  if (is.null(weights)) return(flsa_dp(y, lambda))
+  ## weighted version
+  if (!is.numeric(weights)) {
+    stop("`weights` must be a numeric vector.")
+  }
+  n <- length(y)
+  if (length(weights) != n) {
+    stop("`weights` must have the same length as `y`.")
+  }
+  weights <- weights / sum(weights) * n
+  flsa_dp_weighted(y, lambda, weights)
 }
