80987cd812a1ccacf77bac4f555ce56981d0dabd
(Shirlina) Xuan Li <1208039950@qq.com>
2024-10-21 15:07:12 -0700

    Update test-tvdenoising.R

--- tests/testthat/test-tvdenoising.R
+++ tests/testthat/test-tvdenoising.R
@@ -4,37 +4,45 @@ test_that("tvdenoising works correctly with no weights", {
   y <- c(1, 2, 3, 5, 8, 13, 21)
   lambda <- 1
   result <- tvdenoising(y, lambda)
+  expected_result <- c(2, 2, 3, 5, 8, 13, 20)
   expect_type(result, "double")
   expect_length(result, length(y))
-  
-  # Check if the output is not equal to the input, implying denoising occurred
-  expect_false(all(result == y))
+  expect_equal(result, expected_result, tolerance = 1e-6) 
 })
+
 
 test_that("tvdenoising works correctly with weights", {
   y <- c(1, 2, 3, 5, 8, 13, 21)
   lambda <- 1
-  
-  set.seed(123) 
-  weights <- runif(length(y), min = 0.5, max = 1000)
+
+  weights <- c(1, 3, 3, 2, 5, 6, 2)
   result <- tvdenoising(y, lambda, weights)
   expect_type(result, "double")
   expect_length(result, length(y))
-  
-  # Check if the output is not equal to the input, implying denoising occurred
-  expect_false(all(result == y))
+  expected_result <- c(2,2,3,5,8,13,20.5)
+  expect_equal(result, expected_result, tolerance = 1e-6) 
 })
 
-test_that("tvdenoising with uniform weights matches unweighted version", {
+
+test_that("tvdenoising with uniform weights 1 matches unweighted version", {
   y <- c(1, 2, 3, 5, 8, 13, 21)
   lambda <- 1
-  uniform_weights <- rep(1, length(y))  
-  
+  uniform_weights1 <- rep(1, length(y))  
   result_no_weights <- tvdenoising(y, lambda)
-  result_with_weights <- tvdenoising(y, lambda, uniform_weights)
-
-  expect_equal(result_no_weights, result_with_weights)
+  result_with_weights1 <- tvdenoising(y, lambda, uniform_weights1)
+  expect_equal(result_no_weights, result_with_weights1, tolerance = 1e-6)
 })
+
+test_that("tvdenoising with uniform weights 2", {
+  y <- c(1, 2, 3, 5, 8, 13, 21)
+  lambda <- 1
+  uniform_weights2 <- rep(2, length(y))  
+  expected_result <- c(1.5, 2, 3, 5, 8, 13, 20.5)
+  result_with_weights2 <- tvdenoising(y, lambda, uniform_weights2)
+  expect_equal(expected_result, result_with_weights2, tolerance = 1e-6)
+})
+
+
 
 test_that("tvdenoising handles incorrect input properly", {
   y <- c(1, 2, 3, 5, 8, 13, 21)
